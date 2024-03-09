70ea91610782d5cf8977174e5aabd1ceea1fccdc
Michael Gunkoff <54396113+kaatinga@users.noreply.github.com>
2024-03-09 22:21:53 +0100

    code clean up

---
+++ worker_test.go
@@ -0,0 +1,48 @@
+package luna
+
+import (
+	"testing"
+)
+
+type dummyWorker string
+
+func (v dummyWorker) Start() {}
+
+func (v dummyWorker) Stop() {}
+
+func TestWorkerPool_Get(t *testing.T) {
+	worker := NewWorkerPool[string, dummyWorker]()
+	const key string = "key"
+	const value = dummyWorker("value")
+	worker.Add(key, value)
+
+	t.Run("get existing", func(t *testing.T) {
+		got := worker.Get(key)
+		if got == nil {
+			t.Errorf("got %v, want %v", got, key)
+		}
+
+		if got.Value != value {
+			t.Errorf("got %v, want %v", got.Value, value)
+		}
+
+		if got.Key != key {
+			t.Errorf("got %v, want %v", got.Key, key)
+		}
+	})
+
+	t.Run("get non-existing", func(t *testing.T) {
+		got := worker.Get("non-existing")
+		if got != nil {
+			t.Errorf("got %v, want %v", got, nil)
+		}
+	})
+
+	t.Run("get after delete", func(t *testing.T) {
+		worker.Delete(key)
+		got := worker.Get(key)
+		if got != nil {
+			t.Errorf("got %v, want %v", got, nil)
+		}
+	})
+}
